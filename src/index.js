fddfaef8bf9afa0afa1c3dd014343184a078f050
yuexing <yuexingphp@163.com>
2023-08-18 10:09:51 +0800

    5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React, { Suspense} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App'
-
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
